d3f6bea43abb3e92a1c6e07d46a3b2a23ada7d60
AkshayKaginkar <akshaykaginkar12@gmail.com>
2024-07-05 08:11:30 +0530

    firstCom

--- FRONTEND/makemytrip_front/src/App.js
+++ FRONTEND/makemytrip_front/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router,Route, Routes, RouterProvider  } from 'react-router-dom';
+import { BrowserRouter ,Route, Routes, RouterProvider  } from 'react-router-dom';
 import { router } from './Routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Statecontext from './component/Context/Statecontext';
+import Statecontext from './component/Context/Statecontext'
+import { Main } from './component/HomePage/Main';
+import { Search } from './component/SearchPage/Search';
 
 function App() {
   let apiBaseUrl='http://localhost:5000/api/';
   return (
     <div className="App">
-   
+{/*    
     <Statecontext.Provider
     value={{
       apiBaseUrl
     }}
     >
       <RouterProvider router={router}/>
-      </Statecontext.Provider>
+      </Statecontext.Provider> */}
+         <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/Search" element={<Search />} />
+
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
